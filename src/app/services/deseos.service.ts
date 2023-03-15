e5c9f271e02892d7b345576bdb99aac82507e2b7
Jorge Estenger <jorgeestenger9@gmail.com>
2023-03-14 20:51:09 -0400

    Listas adregadas

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeseosService {
 
-  constructor() { }
+  listas: Lista[]=[];
+
+  constructor() { 
+  const lista1 = new Lista('1st Goals');
+  const lista2 = new Lista('2nd Goals');
+  }
 }
